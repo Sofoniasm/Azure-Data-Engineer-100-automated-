d556549f725ecbec4020dbe98dc5c08e12bbad60
Sofonias Mengistu <69535143+Sofoniasm@users.noreply.github.com>
2025-08-17 21:16:52 +0300

    Add medallion containers and full Terraform project structure

--- terraform/modules/datalake/datalake.tf
+++ terraform/modules/datalake/datalake.tf
@@ -7,3 +7,21 @@ resource "azurerm_storage_account" "datalake" {
   is_hns_enabled           = true
   kind                     = "StorageV2"
 }
+
+resource "azurerm_storage_container" "bronze" {
+  name                  = "bronze"
+  storage_account_name  = azurerm_storage_account.datalake.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "silver" {
+  name                  = "silver"
+  storage_account_name  = azurerm_storage_account.datalake.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "gold" {
+  name                  = "gold"
+  storage_account_name  = azurerm_storage_account.datalake.name
+  container_access_type = "private"
+}
